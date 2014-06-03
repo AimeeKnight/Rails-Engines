e33c7b0cbe01f43081b27f5b4b54b5532f07f9cc
Aimee <AimeeKnight83@gmail.com>
2014-06-02 21:28:49 -0500

    initial commit

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = 'eea6295ad2beb41a2e57fa9e4edf47eadde72b31c8af476b8aee05fd3ae943f69ce8efb39ee84909b815fb1b67cb6d910d35f75329207cf176a26aa40628ec74'
